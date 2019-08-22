ae8e2af09f594d5c82b5f0d9fb35b6f082939e0b
Tim Paul <tim.paul@immutable.com>
2019-08-22 14:59:56 +1000

    mixed items with set rows

--- src/app/virtual-scroll/virtual-scroll.component.ts
+++ src/app/virtual-scroll/virtual-scroll.component.ts
@@ -7,24 +7,35 @@ import { Component, OnInit, Input } from "@angular/core";
 })
 export class VirtualScrollComponent implements OnInit {
   public items: any[] = [];
+  public groupedItems: any[] = [];
   public viewPortItems: any;
 
   constructor() {
     for (let index = 0; index < 100; index++) {
       this.items.push(index);
     }
-
-    this.items.splice(10, 0, "moo");
-    this.items.splice(17, 0, "moo cow");
+    this.groupedItems = groupArr(this.items, 4);
+    this.groupedItems.splice(5, 0, "moo");
+    this.groupedItems.splice(10, 0, "moo cow");
   }
 
   ngOnInit() {}
 
-  public isNumber(input) {
-    return typeof input === "number";
+  public isArray(input) {
+    return Array.isArray(input);
   }
 
   fetchMore(event) {
     // console.log("@@@@@@@@@@@@@@@@", event);
   }
+}
+
+function groupArr(data, n) {
+  var group = [];
+  for (var i = 0, j = 0; i < data.length; i++) {
+    if (i >= n && i % n === 0) j++;
+    group[j] = group[j] || [];
+    group[j].push(data[i]);
+  }
+  return group;
 }
