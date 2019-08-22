9ddce7024c50692c32bb2730f173cf6c39bb31a2
Tim Paul <tim.paul@immutable.com>
2019-08-22 15:22:15 +1000

    virtual scroll working with webcomponents!

--- src/app/virtual-scroll/virtual-scroll.component.ts
+++ src/app/virtual-scroll/virtual-scroll.component.ts
@@ -26,14 +26,16 @@ export class VirtualScrollComponent implements OnInit {
   }
 
   fetchMore(event) {
-    // console.log("@@@@@@@@@@@@@@@@", event);
+    console.log("@@@@@@@@@@@@@@@@", event);
   }
 }
 
 function groupArr(data, n) {
-  var group = [];
-  for (var i = 0, j = 0; i < data.length; i++) {
-    if (i >= n && i % n === 0) j++;
+  const group = [];
+  for (let i = 0, j = 0; i < data.length; i++) {
+    if (i >= n && i % n === 0) {
+      j += 1;
+    }
     group[j] = group[j] || [];
     group[j].push(data[i]);
   }
