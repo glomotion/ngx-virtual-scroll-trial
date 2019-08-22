ea54501aaf8a1e56cd848426932d591f0aa89b7f
Tim Paul <tim.paul@immutable.com>
2019-08-22 14:37:13 +1000

    mixed items demo

--- src/app/virtual-scroll/gallery-img/gallery-img.component.ts
+++ src/app/virtual-scroll/gallery-img/gallery-img.component.ts
@@ -1,23 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'gallery-img',
-  templateUrl: './gallery-img.component.html',
-  styleUrls: ['./gallery-img.component.scss']
+  selector: "gallery-img",
+  templateUrl: "./gallery-img.component.html",
+  styleUrls: ["./gallery-img.component.scss"]
 })
 export class GalleryImgComponent implements OnInit {
-
   @Input() item: any;
   public index;
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngOnInit() {
     this.index = this.item % 4;
     console.log(this.index);
-    
   }
-
 }
--- src/app/virtual-scroll/virtual-scroll.component.ts
+++ src/app/virtual-scroll/virtual-scroll.component.ts
@@ -1,31 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'virtual-scroll-test',
-  templateUrl: './virtual-scroll.component.html',
-  styleUrls: ['./virtual-scroll.component.scss']
+  selector: "virtual-scroll-test",
+  templateUrl: "./virtual-scroll.component.html",
+  styleUrls: ["./virtual-scroll.component.scss"]
 })
 export class VirtualScrollComponent implements OnInit {
-
-  public items: number[]= [];
+  public items: any[] = [];
   public viewPortItems: any;
 
   constructor() {
-
     for (let index = 0; index < 100; index++) {
       this.items.push(index);
     }
 
+    this.items.splice(10, 0, "moo");
+    this.items.splice(17, 0, "moo cow");
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
+  public isNumber(input) {
+    return typeof input === "number";
   }
 
   fetchMore(event) {
-
-    console.log(event);
-
+    // console.log("@@@@@@@@@@@@@@@@", event);
   }
-
 }
